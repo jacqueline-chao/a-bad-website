775dcef10e6d1b04d1b55cde488f8ce49db1df5c
Jacqueline Chao <jacqueline@goarbo.com>
2023-09-29 21:19:36 -0400

    Make some modifications

--- public/app.js
+++ public/app.js
@@ -16,6 +16,10 @@ submit_button.addEventListener('submit', function (e) {
     if (answer_1 == "your name" && answer_2 == "red" && answer_3 == "Fakecapitalia" && acceptable_seqs_nums.includes(answer_4) && answer_5 == "I live on the planet Earth." && answer_6.checked) {
         document.getElementById("results-section").innerText = "You did it!";
     } else {
-        document.getElementById("results-section").innerText = "Incorrect answer(s). Try again!";
+        var hint = "";
+        if (answer_1 != "your name") {
+            hint = "It looks like you answered the first question incorrectly. Here's a hint: Think more LITERALLY about this question."
+        }
+        document.getElementById("results-section").innerText = "Incorrect answer(s). Try again!" + " " + hint;
     }
 });
